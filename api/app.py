57f0b7c4be669f43a540c6cbee1f7860a3f5e840
reprogamaco <76619967+reprogamaco@users.noreply.github.com>
2023-07-06 21:29:15 +0300

    v0.2

--- api/app.py
+++ api/app.py
@@ -1,20 +1,44 @@
 from flask import Flask,render_template,redirect
+from datetime import date
 
 app = Flask(__name__)
 
 mode = 'development'
 # mode = 'production'
 
+# dealing with dates
+today = date.today()
+engDate = today.strftime("%B %d, %Y") # July 01,2023
+year = today.strftime("%Y") # 2023
+
+# website variables
 website_full = 'Yusuph Events planning & Planning'
 website_short = 'yepc'
+slogan = 'Make Your Event Memorable with Us'
 
+# mode of the website
 if mode == 'production':
     home = 'https://yepc.vercel.app/'
 elif mode == 'development':
     home = 'localhost:5000/'
 
 
-# the index page
+# ------------ end of the variables of the website ---------- #
+
+
+
+
+
+
+
+#################################################
+## ******************************************* ##
+# -------- starts of the functions ------------ #
+## ***********codes*and*functions************** #
+#################################################
+
+
+# The index page
 @app.route('/')
 def index():
     return redirect('/en/')
@@ -22,9 +46,49 @@ def index():
 # the home (english) page
 @app.route('/en/')
 def en_home():
-    return render_template('en-home.html')
+    headers = {
+        'title': website_full,
+
+    }
+    return render_template('en-home.html',**context)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+#########################################
+#########################################
+##### server configurations stuffs ######
+#########################################
+#########################################
+
+
+
+
+
+# handling the 404 error
+@app.errorhandler(404)
+def page_404(error):
+    #lets redirect home
+    return redirect('/')
+
+# redirecting to https (443)
+@app.beforerequest
+def forwarding_https():
+    if not request.is_secure:
+        url = request.url.replace('http://','https://',1)
+        code = 301 #redirect code
+        return redirect(url,code=code)
 
 if __name__ == '__main__':
     if mode == 'development':
